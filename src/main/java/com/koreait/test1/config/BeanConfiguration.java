c93243635759c1b4a326db464ab3845e20afea37
soeun1365 <soeun1365@naver.com>
2021-07-12 14:56:41 +0900

    [03]@configuration

--- src/main/java/com/koreait/test1/config/BeanConfiguration.java
+++ src/main/java/com/koreait/test1/config/BeanConfiguration.java
@@ -1,9 +1,11 @@
 package com.koreait.test1.config;
 
 import org.apache.ibatis.session.SqlSessionFactory;
+
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import com.koreait.test1.command.DeleteBoardCommand;
@@ -14,6 +16,7 @@ import com.koreait.test1.command.UpdateBoardCommand;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
+@Configuration
 public class BeanConfiguration {
 	@Bean
 	public HikariConfig hikariConfig() {
